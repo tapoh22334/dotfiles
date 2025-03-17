2cc528928c53a83594825d79107f098086a9cec1
Iwase22334 <iwase@yepes2.local>
2025-03-17 19:05:10 +0900

    bash -> sh

--- setup.sh
+++ setup.sh
@@ -1,45 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 BREW_LIST="./packages/brewlist-full"
 DOTFILES_DIR="./config"
-mapfile -t PACKAGES < <(find "$DOTFILES_DIR" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
-
-# Check if Git is installed, install it if not
-if ! command -v git &> /dev/null; then
-    echo "⚙️ Installing Git..."
-    if [[ "$(uname)" == "Darwin" ]]; then
-        # For macOS, use the system package manager or Xcode command line tools
-        xcode-select --install || brew install git
-    else
-        # For Linux, install Git using the package manager
-        if [[ "$(uname)" == "Linux" ]]; then
-            sudo apt update
-            sudo apt install -y git || sudo yum install -y git
-        fi
-    fi
-fi
 
 # Install Homebrew if not installed
-if ! command -v brew &> /dev/null; then
+if ! which brew >/dev/null 2>&1; then
     echo "🚀 Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Set up Homebrew environment for Linux
-if [[ "$(uname)" == "Linux" ]]; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [ "$(uname)" = "Linux" ]; then
+    eval "$( /home/linuxbrew/.linuxbrew/bin/brew shellenv )"
 fi
 
 # Install packages from the list
 echo "🔧 Installing required packages..."
 brew bundle
 
+# Install stow if not installed
+if ! which stow >/dev/null 2>&1; then
+    echo "🚀 Installing stow..."
+    brew install stow
+fi
+
 # Stow dotfiles
 echo "📂 Applying dotfiles configuration..."
 
-stow -v -t "$HOME" -d "$DOTFILES_DIR" "${PACKAGES[@]}"
+PACKAGES=""
+find "$DOTFILES_DIR" -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | while read -r line; do
+    PACKAGES="$PACKAGES $line"
+done
+
+stow -v -t "$HOME" -d "$DOTFILES_DIR" $PACKAGES
 
 echo "✨ Dotfiles setup complete!"
 
